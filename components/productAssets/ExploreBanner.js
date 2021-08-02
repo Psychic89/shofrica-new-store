56802f18433bc4d14a5fa8404f264ef439790ff0
Robert Mutua <robert@shofrica.com>
2021-08-02 16:45:03 +0200

    second commit

--- components/productAssets/ExploreBanner.js
+++ components/productAssets/ExploreBanner.js
@@ -38,7 +38,7 @@ export default class ExploreBanner extends React.Component {
           (window.innerHeight - dimensions.top) / window.innerHeight - 1;
 
         this.image.current.style.transform = `translateY(${-scrolledRatio *
-          100}px)`;
+        100}px)`;
       }
     }
   }
@@ -46,48 +46,48 @@ export default class ExploreBanner extends React.Component {
   render() {
     return (
       <div className="py-5 mb-5 explore-banner">
-        <div className="bg-brand300 position-relative py-md-5">
-          {/* Image Absolute */}
-          <div className="position-md-absolute left-0 bottom-0 right-0">
-            <div className="custom-container px-0">
-              <div className="row">
-                <div className="col-md-5 offset-md-7">
-                  <div className="position-relative">
-                    <div className="position-md-absolute right-0 left-0 bottom-0">
-                      <div
-                        ref={this.image}
-                        className="explore-banner--image"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* Content */}
-          <div ref={this.exploreContainer} className="custom-container py-md-5">
-            <div className="row py-5">
-              <div className="col-12 col-md-6 py-5">
-                <p
-                  className="font-size-display3 font-weight-light mb-4"
-                  style={{ maxWidth: '20rem' }}
-                >
-                  A new shopping experience
-                </p>
-                <div className="d-flex">
-                  <Link href="/collection">
-                    <a className="d-flex py-3 align-items-center font-color-black borderbottom border-color-black">
-                      <p className="mr-3">Explore products</p>
-                      <img src="/icon/arrow-long-right.svg" />
-                    </a>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="bg-brand300 position-relative py-md-5">
+      {/* Image Absolute */}
+      <div className="position-md-absolute left-0 bottom-0 right-0">
+      <div className="custom-container px-0">
+      <div className="row">
+      <div className="col-md-5 offset-md-7">
+      <div className="position-relative">
+      <div className="position-md-absolute right-0 left-0 bottom-0">
+      <div
+    ref={this.image}
+    className="explore-banner--image"
+      />
       </div>
-    );
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+
+    {/* Content */}
+  <div ref={this.exploreContainer} className="custom-container py-md-5">
+      <div className="row py-5">
+      <div className="col-12 col-md-6 py-5">
+      <p
+    className="font-size-display3 font-weight-light mb-4"
+    style={{ maxWidth: '20rem' }}
+  >
+    Giving a gift?
+  </p>
+    <div className="d-flex">
+      <Link href="/giftguide">
+      <a className="d-flex py-3 align-items-center font-color-black borderbottom border-color-black">
+      <p className="mr-3">How about one she’ll (actually) love?</p>
+    <img src="/icon/arrow-long-right.svg" />
+      </a>
+      </Link>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+  );
   }
 }
